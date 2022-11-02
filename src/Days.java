1a99cddb849af1ad149fa22dfed669c52d783de0
kurmanbek89 <kumakary2@gmail.com>
2022-11-03 00:49:06 +0300

    sd

--- src/Days.java
+++ src/Days.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public enum Days {
+public enum Days {
+    MONDAY("Duishombu"),
+    TUESDAY("Beishembi"),
+    WEDNESDAY("Sharshembi"),
+    THURSDAY("Beishenbi"),
+    FRIDAY("Juma"),
+    SATURDAY("Ishembi"),
+    SUNDAY("Jekshembi");
+
+    String name;
+
+    Days(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Days{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        Days days = Days.valueOf(s.toUpperCase());
+        switch (days) {
+            case MONDAY, WEDNESDAY, FRIDAY -> System.out.println(days.getName() + " kunu uchim Java");
+            case TUESDAY -> System.out.println(days.getName() + " kunu uchim English i Java practise");
+            case THURSDAY -> System.out.println(days.getName() + " kunu uchim Soft Skills i Java practice");
+            case SATURDAY -> System.out.println(days.getName() + " kunu tolko Java practice");
+            case SUNDAY -> System.out.println(days.getName() + " kunu we have a rest");
+        }
     }
 }
